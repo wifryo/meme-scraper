5b7d0a8ab4a9f8c556a0b4b5037412b4e4433975
William Young <wifryo@gmail.com>
2022-09-09 15:11:39 +0200

    Add meme generator function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import cliProgress from 'cli-progress';
 
-const url = 'https://memegen-link-examples-upleveled.netlify.app/';
+let url;
 const mq = 10;
 
 // Initialise progress bar & calculate how much to increment it in the loop later
@@ -23,6 +23,41 @@ try {
 }
 
 bar.update(20);
+
+// condition ? if true : if false
+// if there is an argv[2], string 1 is argv[2], otherwise it is undefined
+const string1 = process.argv[2] ? process.argv[2] : undefined;
+// if there is an argv[3], string 2 is argv[3], otherwise it is '_'
+const string2 = process.argv[3] ? process.argv[3] : '_';
+
+// if there is an argv[4], string 3 is argv[4], otherwise it is '_'
+const string3 = process.argv[4] ? process.argv[4] : '_';
+// if both string1 and string2 exist text is string1/string2, else text is string1
+let text;
+if (string1 && !string2) {
+  text = string1;
+} else if (string1 && string2 && !string3) {
+  text = `/${string1}/${string2}`;
+} else if (string1 && string2 && string3) {
+  text = `/${string1}/${string2}/${string3}`;
+}
+
+async function memeGen() {
+  try {
+    // Extract image source
+    const image = await axios({
+      url: url,
+      method: 'GET',
+      responseType: 'stream',
+    });
+    bar.update(50);
+    const path = `./memes/${string1}.jpg`;
+    const writer = fs.createWriteStream(path);
+    image.data.pipe(writer);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 // Function to scrape & store defined number of memes
 async function scrapeMemes() {
@@ -56,6 +91,13 @@ async function scrapeMemes() {
   }
 }
 
-await scrapeMemes();
+if (string1 === undefined) {
+  url = 'https://memegen-link-examples-upleveled.netlify.app/';
+  await scrapeMemes();
+} else {
+  url = `https://api.memegen.link/images${text}.jpg`;
+  await memeGen();
+}
+
 bar.update(100);
 bar.stop();
