daa22b3138c9b395e811dbf043ce0c26a45b4dfb
William <wifryo@gmail.com>
2022-09-09 06:00:56 +0200

    Add progress bar

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import fs from 'node:fs';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import cliProgress from 'cli-progress';
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 const mq = 10;
 
+// Initialise progress bar & calculate how much to increment it in the loop later
+const barStep = 80 / mq;
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_grey);
+bar.start(100, 0);
+bar.update(10);
+
+// Create directory
 const folderName = './memes';
 try {
   if (!fs.existsSync(folderName)) {
@@ -14,7 +22,10 @@ try {
   console.error(err);
 }
 
-async function scrapeData() {
+bar.update(20);
+
+// Function to scrape & store defined number of memes
+async function scrapeMemes() {
   try {
     // Fetch HTML of the target website
     const { data } = await axios.get(url);
@@ -26,21 +37,25 @@ async function scrapeData() {
       const image = $(this).attr('src');
       arr.push(image);
     });
-    // Do something to each of the first 10 image sources
+    // Get image data
     for (let i = 0; i < mq; i++) {
       const image = await axios({
         url: arr[i],
         method: 'GET',
         responseType: 'stream',
       });
+      // Write image data to file
       const path = `./memes/img0${i}.jpg`;
       const writer = fs.createWriteStream(path);
-
       image.data.pipe(writer);
+
+      bar.update(barStep);
     }
   } catch (err) {
     console.error(err);
   }
 }
 
-scrapeData();
+scrapeMemes();
+bar.update(100);
+bar.stop();
